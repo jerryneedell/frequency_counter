fc68fc94d46f869cedbf0d64e3c4681e3322a048
jerryneedell <jerryneedell@gmail.com>
2022-03-06 15:46:13 -0500

    fix contol-c -- fix double space -- write to Desktop

--- fc_log.py
+++ fc_log.py
@@ -2,6 +2,7 @@ import serial
 import time
 import os
 import sys
+from pathlib import Path
 
 port = ''
 
@@ -14,28 +15,40 @@ port = sys.argv[1]
 ser = serial.Serial(port, 115200)
 file_count = 0
 file_open = False
-while True:
-    #data = ser.read(ser.inWaiting())
-    data = ser.read_until(expected=b'\n')
-    #print(data)
-    if data == b'Started\r\n' :
-        now = time.localtime()
-        tag = "_".join([str(x) for x in now[0:6]])
-        filename = "fc_log_"+tag+".csv"
-        f = open(filename,"w")
-        f.close()
-        file_open = True
-        data = None
-        file_count += 1
-    if file_open and data:
-        #if data[len(data)-1] != 0 :
-        #    print("null terminator missing\rn")
-        #print([hex(x) for x in data])
-        print(data)
-        try:
-            with open(filename,"a") as logfile:
-                logfile.write(data.decode("UTF-8"))
-        except UnicodeDecodeError as e:
-            print("decode error",e)
-    else:
-        print(".",end='',flush=True)
+try:
+    while True:
+        #data = ser.read(ser.inWaiting())
+        data = ser.read_until(expected=b'\n')
+        #print(data)
+        if data == b'Started\r\n' :
+            now = time.localtime()
+            path = str(Path.home())+"/Desktop/"
+            tag = "_".join([str(x) for x in now[0:6]])
+            filename = path+"fc_log_"+tag+".csv"
+            f = open(filename,"w")
+            f.close()
+            file_open = True
+            data = None
+            file_count += 1
+        if file_open and data:
+            #if data[len(data)-1] != 0 :
+            #    print("null terminator missing\rn")
+            #print([hex(x) for x in data])
+            print(data)
+            line = data[0:-2].decode("UTF-8")
+            print(line)
+            line = line + '\n'
+            print(line)
+            try:
+                with open(filename,"a") as logfile:
+                    #logfile.write(data.decode("UTF-8"))
+                    logfile.write(line)
+            except UnicodeDecodeError as e:
+                print("decode error",e)
+        else:
+            print(".",end='',flush=True)
+
+except KeyboardInterrupt:
+    pass
+except Exception as e:
+    print("Som errror occured ",e)
